ce2c124890f152908183a3e2d2a8b0091ec11889
Shayan Salehi <79784127+ShayanSalehi81@users.noreply.github.com>
2024-08-29 09:49:14 +0330

    Create DatasetCreator.py

---
+++ Scripts/DatasetCreator.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def create_dataset(dataset_path):
+    train_df = pd.read_csv(dataset_path)
+    result_df = pd.DataFrame({
+        'Context': train_df['Context'],
+        'Response': train_df['Response'],
+        'Context_Translated': pd.NA,
+        'Response_Translated': pd.NA
+    })
+    result_df.to_csv('Dataset/result.csv')
+
+if __name__ == '__main__':
+    create_dataset(dataset_path='Dataset/train.csv')
